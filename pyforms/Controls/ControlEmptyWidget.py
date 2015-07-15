c4a61128bb0387842e39188e2bc0ef644648192c
Carlos Mão de Ferro <carlos@macmini.benfica>
2015-07-15 16:56:30 +0100

    PEP 8 compliance

--- pyforms/Controls/ControlEmptyWidget.py
+++ pyforms/Controls/ControlEmptyWidget.py
@@ -1,50 +1,51 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-__author__      = "Ricardo Ribeiro"
-__credits__     = ["Ricardo Ribeiro"]
-__license__     = "MIT"
-__version__     = "0.0"
-__maintainer__  = "Ricardo Ribeiro"
-__email__       = "ricardojvr@gmail.com"
-__status__      = "Development"
+'''
+@author: Ricardo Ribeiro
+@credits: Ricardo Ribeiro
+@license: MIT
+@version: 0.0
+@maintainer: Ricardo Ribeiro
+@email: ricardojvr@gmail.com
+@status: Development
+@lastEditedBy: Carlos Mão de Ferro (carlos.maodeferro@neuro.fchampalimaud.org)
+'''
 
-
-import pyforms.Utils.tools as tools
-from PyQt4 import uic, QtGui
+from PyQt4 import QtGui
 from pyforms.Controls.ControlBase import ControlBase
+
 
 class ControlEmptyWidget(ControlBase):
 
     def initControl(self):
-        
+
         self._form = QtGui.QWidget()
         layout = QtGui.QVBoxLayout()
         layout.setMargin(0)
-        self._form.setLayout( layout )
+        self._form.setLayout(layout)
 
-    ############################################################################
-    ############ Properties ####################################################
-    ############################################################################
+    ##########################################################################
+    ############ Properties ##################################################
+    ##########################################################################
 
     @property
     def value(self): return ControlBase.value.fget(self)
 
     @value.setter
     def value(self, value):
-        if isinstance( self._value, list ):
+        if isinstance(self._value, list):
             for w in self._value:
-                if w!=None and w!="": self._form.layout().removeWidget( w._form )
+                if w is not None and w != "":
+                    self._form.layout().removeWidget(w._form)
         else:
-            if self._value!=None and self._value!="": self._form.layout().removeWidget( self._value._form )
+            if self._value is not None and self._value != "":
+                self._form.layout().removeWidget(self._value._form)
 
-        if isinstance( value, list ):
+        if isinstance(value, list):
             for w in value:
-                self._form.layout().addWidget( w._form )
+                self._form.layout().addWidget(w._form)
         else:
-            self._form.layout().addWidget( value._form )
+            self._form.layout().addWidget(value._form)
 
-        ControlBase.value.fset(self,value)
-        
-        
-    
+        ControlBase.value.fset(self, value)
