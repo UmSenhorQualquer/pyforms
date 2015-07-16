7418d4e2cbecdbb1048ebdbfd304d98176edf5b3
Carlos Mão de Ferro <carlos@macmini.benfica>
2015-07-16 10:58:33 +0100

    PEP 8 compliance

--- pyforms/Controls/ControlDockWidget.py
+++ pyforms/Controls/ControlDockWidget.py
@@ -1,34 +1,44 @@
-import pyforms.Utils.tools as tools
-from PyQt4 import uic, QtGui,QtCore
+#!/usr/bifn/python
+# -*- coding: utf-8 -*-
+'''
+@author: Ricardo Ribeiro
+@credits: Ricardo Ribeiro
+@license: MIT
+@version: 0.0
+@maintainer: Ricardo Ribeiro
+@email: ricardojvr@gmail.com
+@status: Development
+@lastEditedBy: Carlos Mão de Ferro (carlos.maodeferro@neuro.fchampalimaud.org)
+'''
+
 from pyforms.Controls.ControlEmptyWidget import ControlEmptyWidget
+
 
 class ControlDockWidget(ControlEmptyWidget):
 
-	SIDE_LEFT 		= 'left'
-	SIDE_RIGHT 		= 'right'
-	SIDE_TOP 		= 'top'
-	SIDE_BOTTOM 	= 'bottom'
-	SIDE_DETACHED 	= 'detached'
+    SIDE_LEFT = 'left'
+    SIDE_RIGHT = 'right'
+    SIDE_TOP = 'top'
+    SIDE_BOTTOM = 'bottom'
+    SIDE_DETACHED = 'detached'
 
-	def __init__(self, label='', side='left'):
-		super(ControlDockWidget, self).__init__(label)
-		self.side = side
+    def __init__(self, label='', side='left'):
+        super(ControlDockWidget, self).__init__(label)
+        self.side = side
 
-	@property
-	def label(self): return self._label
+    @property
+    def label(self): return self._label
 
-	@label.setter
-	def label(self, value): 
-		self._label = value
-		self.dock.setWindowTitle(value)
+    @label.setter
+    def label(self, value):
+        self._label = value
+        self.dock.setWindowTitle(value)
 
+    def save(self, data):
+        data['side'] = self.side
+        super(ControlDockWidget, self).save(data)
 
-	def save(self, data):
-		data['side']=self.side
-		super(ControlDockWidget, self).save(data)
-
-	def load(self, data):
-		self.side = data['side']
-		print data
-		super(ControlDockWidget, self).load(data)
-
+    def load(self, data):
+        self.side = data['side']
+        # print(data)
+        super(ControlDockWidget, self).load(data)
--- pyforms/Controls/ControlTree.py
+++ pyforms/Controls/ControlTree.py
@@ -1,130 +1,126 @@
 from pyforms.Controls.ControlBase import ControlBase
-from PyQt4 import uic
-from PyQt4 import QtGui, QtCore
-
-class ControlTree(ControlBase,QtGui.QTreeWidget):
-
-	def __init__(self, label='', default=''):
-		ControlBase.__init__(self,label, default)
-		QtGui.QTreeWidget.__init__(self)
+from PyQt4 import QtGui
 
 
-	def initForm(self):
-		self.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
-		self.setUniformRowHeights(True)
-		self.setDragDropMode(QtGui.QAbstractItemView.InternalMove)
-		self.setDragEnabled(True)
-		self.setAcceptDrops(True)
-		
-		self.model().dataChanged.connect(self.__itemChangedEvent)
-		
-		self.selectionChanged = self.selectionChanged
+class ControlTree(ControlBase, QtGui.QTreeWidget):
 
-		
+    def __init__(self, label='', default=''):
+        ControlBase.__init__(self, label, default)
+        QtGui.QTreeWidget.__init__(self)
 
-	@property
-	def showHeader(self): return self.header().isVisible()
-	@showHeader.setter
-	def showHeader(self, value): 
-		if value: 
-			self.header().show()
-		else: 
-			self.header().hide()
+    def initForm(self):
+        self.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+        self.setUniformRowHeights(True)
+        self.setDragDropMode(QtGui.QAbstractItemView.InternalMove)
+        self.setDragEnabled(True)
+        self.setAcceptDrops(True)
 
+        self.model().dataChanged.connect(self.__itemChangedEvent)
 
-	def __itemChangedEvent(self, item): self.itemChangedEvent(item)
+        self.selectionChanged = self.selectionChanged
 
-	def itemChangedEvent(self, item): pass
-		
+    @property
+    def showHeader(self): return self.header().isVisible()
 
-	def itemSelectionChanged(self):pass
+    @showHeader.setter
+    def showHeader(self, value):
+        if value:
+            self.header().show()
+        else:
+            self.header().hide()
 
-	def selectionChanged(self, selected, deselected ):
-		super(QtGui.QTreeView, self).selectionChanged(selected, deselected)
-		self.itemSelectionChanged()
+    def __itemChangedEvent(self, item): self.itemChangedEvent(item)
 
-	@property
-	def mouseSelectedRowsIndexes(self):
-		result = []
-		for index in self.selectedIndexes():
-			result.append( index.row() )
-		return list( set(result) )
+    def itemChangedEvent(self, item): pass
 
+    def itemSelectionChanged(self): pass
 
-	@property
-	def mouseSelectedRowIndex(self):
-		indexes = self.mouseSelectedRowsIndexes
-		if len(indexes)>0: return indexes[0]
-		else: return None
+    def selectionChanged(self, selected, deselected):
+        super(QtGui.QTreeView, self).selectionChanged(selected, deselected)
+        self.itemSelectionChanged()
 
-	@property
-	def selectedItem(self):
-		for index in self.selectedIndexes():
-			item = index.model().itemFromIndex(index)
-			return item
-		else:
-			return None
+    @property
+    def mouseSelectedRowsIndexes(self):
+        result = []
+        for index in self.selectedIndexes():
+            result.append(index.row())
+        return list(set(result))
 
-	@property
-	def cells(self): 
-		results = []
-		for row in range(self.model().rowCount()):
-			r = []
-			for col in range(self.model().columnCount()):
-				r.append( self.model.item(row, col) )
-				"""
+    @property
+    def mouseSelectedRowIndex(self):
+        indexes = self.mouseSelectedRowsIndexes
+        if len(indexes) > 0:
+            return indexes[0]
+        else:
+            return None
+
+    @property
+    def selectedItem(self):
+        for index in self.selectedIndexes():
+            item = index.model().itemFromIndex(index)
+            return item
+        else:
+            return None
+
+    @property
+    def cells(self):
+        results = []
+        for row in range(self.model().rowCount()):
+            r = []
+            for col in range(self.model().columnCount()):
+                r.append(self.model.item(row, col))
+                """
 				try:
 					r.append( self._model.item(col, row) )
 				except:
 					r.append( None )
 					pass"""
-			if len(r)>0: results.append(r)
+            if len(r) > 0:
+                results.append(r)
 
-		return results
+        return results
 
+    def __add__(self, other):
+        if isinstance(other, QtGui.QTreeWidgetItem):
+            self.model().invisibleRootItem().appendRow(other)
 
-	def __add__(self, other):
-		if isinstance(other, QtGui.QTreeWidgetItem):
-			self.model().invisibleRootItem().appendRow( other )
+        elif isinstance(other, list):
+            for x in other:
+                # print(x)
+                item = QtGui.QTreeWidgetItem(x)
+                self.form.addTopLevelItem(item)
+        else:
+            item = QtGui.QTreeWidgetItem(other)
+            self.form.addTopLevelItem(item)
 
-		elif isinstance(other, list):
-			for x in other:
-				print x
-				item = QtGui.QTreeWidgetItem( x )
-				self.form.addTopLevelItem(item)
-		else:
-			item = QtGui.QTreeWidgetItem( other )
-			self.form.addTopLevelItem(item)
+        self.setFirstColumnSpanned(
+            self.model().rowCount() - 1, self.rootIndex(), True)
+        return self
 
-		self.setFirstColumnSpanned(self.model().rowCount()-1, self.rootIndex(), True)
-		return self
+    def __sub__(self, other):
+        if isinstance(other, int):
+            if other < 0:
+                indexToRemove = self.mouseSelectedRowIndex
+            else:
+                indexToRemove = other
+            self.model().removeRow(indexToRemove)
+        return self
 
-	def __sub__(self, other):
-		if isinstance(other, int):
-			if other < 0:
-				indexToRemove = self.mouseSelectedRowIndex
-			else:
-				indexToRemove = other
-			self.model().removeRow(indexToRemove)
-		return self
+    @property
+    def form(self): return self
 
-	@property
-	def form(self): return self
-		
+    @property
+    def value(self):
+        return self.model().invisibleRootItem()
+        return self.recursivelyReadRoot(root)
 
-	@property
-	def value(self):  
-		return self.model().invisibleRootItem()
-		return self.recursivelyReadRoot(root)
+    @value.setter
+    def value(self, value):
+        for row in value:
+            self += row
 
-	@value.setter
-	def value(self, value):
-		for row in value: self += row
+    def getAllSceneObjects(self): return self.model().getChildrens()
 
+    def save(self, data): pass
 
-	def getAllSceneObjects(self): return self.model().getChildrens()
-
-
-
-	def save(self, data): pass
-	def load(self, data): pass
+    def load(self, data): pass
