98e8e58b188336ad49e24cfaa1aaa46ca9de24cc
Carlos Mão de Ferro <carlos@macmini.benfica>
2015-07-17 17:02:42 +0100

    PEP 8 compliance fixed mistyped variable

--- pyforms/Controls/ControlButton.py
+++ pyforms/Controls/ControlButton.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-__author__      = "Ricardo Ribeiro"
-__credits__     = ["Ricardo Ribeiro"]
-__license__     = "MIT"
-__version__     = "0.0"
-__maintainer__  = "Ricardo Ribeiro"
-__email__       = "ricardojvr@gmail.com"
-__status__      = "Development"
-
+'''
+@author: Ricardo Ribeiro
+@credits: Ricardo Ribeiro
+@license: MIT
+@version: 0.0
+@maintainer: Ricardo Ribeiro
+@email: ricardojvr@gmail.com
+@status: Development
+@lastEditedBy: Carlos Mão de Ferro (carlos.maodeferro@neuro.fchampalimaud.org)
+'''
 
 import pyforms.Utils.tools as tools
-from PyQt4 import uic, QtGui
+from PyQt4 import uic
 from pyforms.Controls.ControlBase import ControlBase
+
 
 class ControlButton(ControlBase):
 
     def initForm(self):
-        control_path = tools.getFileInSameDirectory(__file__,"button.ui")
-        self._form = uic.loadUi( control_path )
+        control_path = tools.getFileInSameDirectory(__file__, "button.ui")
+        self._form = uic.loadUi(control_path)
         self._form.pushButton.setText(self._label)
 
     def load(self, data): pass
 
     def save(self, data): pass
 
-    ############################################################################
+    ##########################################################################
 
     @property
-    def label(self): return ControlBase.lable.fget(self)
+    def label(self):
+        return ControlBase.label.fget(self)
 
     @label.setter
-    def label(self, value): 
+    def label(self, value):
         ControlBase.label.fset(self, value)
         self._form.pushButton.setText(self._label)
 
-    ############################################################################
-    
+    ##########################################################################
+
     @property
-    def value(self): return None
+    def value(self):
+        return None
 
     @value.setter
     def value(self, value):
