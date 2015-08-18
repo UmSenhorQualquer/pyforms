90842eb9633f09e4d115f2adf7f64152db6da19d
Carlos Mão de Ferro <carlos@macmini.benfica>
2015-08-18 18:31:53 +0100

    refactoring ControlMdiArea

--- pyforms/gui/Controls/ControlMdiArea.py
+++ pyforms/gui/Controls/ControlMdiArea.py
@@ -4,6 +4,7 @@
 """ pyforms.gui.Controls.ControlMdiArea"""
 
 from PyQt4 import QtGui
+from PyQt4.QtCore import Qt
 from pyforms.gui.Controls.ControlBase import ControlBase
 
 __author__ = "Carlos Mão de Ferro"
@@ -24,6 +25,7 @@ class ControlMdiArea(ControlBase):
 
     def initForm(self):
         self._form = QtGui.QMdiArea()
+        ControlBase.value.fset(self, [])
 
     ##########################################################################
     ############ Properties ##################################################
@@ -34,19 +36,34 @@ class ControlMdiArea(ControlBase):
 
     @value.setter
     def value(self, value):
-        if isinstance(self._value, list):
-            for w in self._value:
+        """" @value: list of widgets to be added as subwindows"""
+        # if isinstance(self._value, list):
+        #    for w in self._value:
 
-                if w is not None and w != "":
-                    self._form.addSubWindow(w.form)
-        else:
-            if self._value is not None and self._value != "":
-                self._form.addSubWindow(value.form)
+        #        if w is not None and w != "":
+        #            self._form.addSubWindow(w.form)
+        # else:
+        #    if self._value is not None and self._value != "":
+        #        self._form.addSubWindow(value.form)
+        for widget in value:
+            if widget not in self._value:
+                subWindow = QtGui.QMdiSubWindow()
+                subWindow.setWidget(widget.form)
+                subWindow.setAttribute(Qt.WA_DeleteOnClose)
+                self._form.addSubWindow(subWindow)
+                subWindow.hide()
 
-        if isinstance(value, list):
-            for w in value:
-                self._form.addSubWindow(w.form)
-        else:
-            self._form.addSubWindow(value.form)
+                #subWindow.closeEvent = self._subWindowClosed
 
         ControlBase.value.fset(self, value)
+
+    def showSubWindow(self, widget):
+        for subWindow in self._form.subWindowList():
+            if subWindow.widget() == widget:
+                subWindow.show()
+
+    # def _subWindowClosed(self, closeEvent):
+    #    activeWidget = self._form.activeSubWindow().widget()
+    #    if activeWidget in self._value:
+    #        self._value.remove(activeWidget)
+    #    closeEvent.accept()
