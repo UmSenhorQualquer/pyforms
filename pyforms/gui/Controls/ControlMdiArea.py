6b87485372a001797f462ee283c56bf32e8c84be
Carlos Mão de Ferro <carlos@macmini.benfica>
2015-08-18 11:03:08 +0100

    PEP8 compliance Enhanced documentation

--- pyforms/gui/Controls/ControlMdiArea.py
+++ pyforms/gui/Controls/ControlMdiArea.py
@@ -1,45 +1,51 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__author__      = "Ricardo Ribeiro"
-__credits__     = ["Ricardo Ribeiro"]
-__license__     = "MIT"
-__version__     = "0.0"
-__maintainer__  = "Ricardo Ribeiro"
-__email__       = "ricardojvr@gmail.com"
-__status__      = "Development"
+""" pyforms.gui.Controls.ControlMdiArea"""
 
-from PyQt4 import uic, QtGui
+from PyQt4 import QtGui
 from pyforms.gui.Controls.ControlBase import ControlBase
-import pyforms.Utils.tools as tools
+
+__author__ = "Carlos Mão de Ferro"
+__copyright__ = ""
+__credits__ = "Carlos Mão de Ferro"
+__license__ = "MIT"
+__version__ = "0.0"
+__maintainer__ = ["Ricardo Ribeiro", "Carlos Mão de Ferro"]
+__email__ = ["ricardojvr at gmail.com", "cajomferro at gmail.com"]
+__status__ = "Development"
+
 
 class ControlMdiArea(ControlBase):
+    """
+    The ControlMdiArea wraps a QMdiArea widget which provides
+     an area in which MDI windows are displayed.
+    """
 
     def initForm(self):
         self._form = QtGui.QMdiArea()
-        
-    ############################################################################
-    ############ Properties ####################################################
-    ############################################################################
+
+    ##########################################################################
+    ############ Properties ##################################################
+    ##########################################################################
 
     @property
     def value(self): return ControlBase.value.fget(self)
 
     @value.setter
     def value(self, value):
-        if isinstance( self._value, list ):
+        if isinstance(self._value, list):
             for w in self._value:
-                if w!=None and w!="": self._form.layout().removeWidget( w._form )
+                if w is not None and w is not "":
+                    self._form.layout().removeWidget(w._form)
         else:
-            if self._value!=None and self._value!="": self._form.layout().removeWidget( self._value._form )
+            if self._value is not None and self._value is not "":
+                self._form.layout().removeWidget(self._value._form)
 
-        if isinstance( value, list ):
+        if isinstance(value, list):
             for w in value:
-                self._form.layout().addWidget( w._form )
+                self._form.layout().addWidget(w._form)
         else:
-            self._form.layout().addWidget( value._form )
+            self._form.layout().addWidget(value._form)
 
-        ControlBase.value.fset(self,value)
-        
-        
-    
+        ControlBase.value.fset(self, value)
