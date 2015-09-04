8c3c313231d1299b2fb6f2b1079db3cb9cd94820
Carlos Mão de Ferro <carlos@macmini.benfica>
2015-09-04 14:57:39 +0100

    PEP8 compliance set/get read only property

--- pyforms/gui/Controls/ControlTextArea.py
+++ pyforms/gui/Controls/ControlTextArea.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__author__      = "Ricardo Ribeiro"
-__credits__     = ["Ricardo Ribeiro"]
-__license__     = "MIT"
-__version__     = "0.0"
-__maintainer__  = "Ricardo Ribeiro"
-__email__       = "ricardojvr@gmail.com"
-__status__      = "Development"
-
-
-import os
-import pickle
 import pyforms.Utils.tools as tools
-from PyQt4 import uic, QtGui
+from PyQt4 import uic
 from pyforms.gui.Controls.ControlBase import ControlBase
+
+__author__ = "Ricardo Ribeiro"
+__credits__ = ["Ricardo Ribeiro"]
+__license__ = "MIT"
+__version__ = "0.0"
+__maintainer__ = "Ricardo Ribeiro"
+__email__ = "ricardojvr@gmail.com"
+__status__ = "Development"
+
 
 class ControlTextArea(ControlBase):
 
     def initForm(self):
-        control_path = tools.getFileInSameDirectory(__file__,"textArea.ui")
-        self._form = uic.loadUi( control_path )
+        control_path = tools.getFileInSameDirectory(__file__, "textArea.ui")
+        self._form = uic.loadUi(control_path)
         self._form.label.setText(self._label)
         self._form.plainTextEdit.setPlainText(self._value)
 
-    ############################################################################
-    ############ Properties ####################################################
-    ############################################################################
-
     @property
-    def value(self): 
+    def value(self):
         self._value = str(self._form.plainTextEdit.toPlainText())
         return self._value
 
     @value.setter
     def value(self, value):
-        ControlBase.value.fset(self,value)
+        ControlBase.value.fset(self, value)
         self._form.plainTextEdit.setPlainText(self._value)
+
+    @property
+    def readOnly(self):
+        return self._form.plainTextEdit.isReadOnly()
+
+    @readOnly.setter
+    def readOnly(self, value):
+        self._form.plainTextEdit.setReadOnly(value)
