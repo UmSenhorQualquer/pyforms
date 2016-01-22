b34b2ee85284b6b3152422364c4fd35411146b75
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2016-01-22 10:31:57 +0000

    More on scintilla editor

--- pyforms/gui/Controls/ControlCodeEditor.py
+++ pyforms/gui/Controls/ControlCodeEditor.py
@@ -14,32 +14,52 @@
 from pyforms.gui.Controls.ControlBase import ControlBase
 import pyforms.Utils.tools as tools
 from PyQt4 import uic
+from PyQt4.Qsci import QsciScintilla, QsciLexerPython
+from PyQt4.QtGui import QFontMetrics
+from PyQt4.QtGui import QFont
+from PyQt4.QtGui import QColor
+
 
 class ControlCodeEditor(ControlBase):
 
-	def initForm(self):
-		control_path = tools.getFileInSameDirectory(__file__, "code_editor.ui")
-		self._form = uic.loadUi(control_path)
-		# self.form.label.setText(self._label)
-		# self.form.lineEdit.setText(self._value)
-		# self.form.setToolTip(self.help)
+    def initForm(self):
+        control_path = tools.getFileInSameDirectory(__file__, "code_editor.ui")
+        self._form = uic.loadUi(control_path)
+        # self.form.label.setText(self._label)
+        # self.form.lineEdit.setText(self._value)
+        # self.form.setToolTip(self.help)
 
-		super(ControlCodeEditor, self).initForm()
-		# self.form.lineEdit.editingFinished.connect(self.finishEditing)
+        # Set the default font
+        font = QFont()
+        font.setFamily('Courier')
+        font.setFixedPitch(True)
+        font.setPointSize(10)
+        self._form.code_area.setFont(font)
+        self._form.code_area.setMarginsFont(font)
+
+        # Margin 0 is used for line numbers
+        fontmetrics = QFontMetrics(font)
+        self._form.code_area.setMarginsFont(font)
+        self._form.code_area.setMarginWidth(0, fontmetrics.width("00000") + 6)
+        self._form.code_area.setMarginLineNumbers(0, True)
+        self._form.code_area.setMarginsBackgroundColor(QColor("#cccccc"))
+
+        super(ControlCodeEditor, self).initForm()
+        # self.form.lineEdit.editingFinished.connect(self.finishEditing)
 
 #	def finishEditing(self):
 #		"""Function called when the lineEdit widget is edited"""
 #		self.changed()
 #		self.form.lineEdit.focusNextChild()
 
-	###################################################################
-	############ Properties ###########################################
-	###################################################################
+    ###################################################################
+    ############ Properties ###########################################
+    ###################################################################
 
-	@property
-	def value(self):
-		return self._form.code_area.toPlainText()
-
-	@value.setter
-	def value(self, value):
-		self._form.code_area.setPlainText(str(value))
+#     @property
+#     def value(self):
+#         return self._form.code_area.toPlainText()
+#
+#     @value.setter
+#     def value(self, value):
+#         self._form.code_area.setPlainText(str(value))
