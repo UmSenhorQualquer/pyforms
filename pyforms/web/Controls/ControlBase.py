593f7613571c4d86dee47bf0661a6695dfcfe53d
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2016-05-05 15:28:49 +0100

    force label to str

--- pyforms/web/Controls/ControlBase.py
+++ pyforms/web/Controls/ControlBase.py
@@ -24,10 +24,10 @@ class ControlBase(object):
 
     def serialize(self):
         return { 
-            'name':     self.__class__.__name__, 
+            'name':     str(self.__class__.__name__), 
             'value':    str(self.value),
-            'label':    self._label if self._label else '',
-            'help':     self._help if self._help else '',
+            'label':    str(self._label if self._label else ''),
+            'help':     str(self._help if self._help else ''),
             'visible':  int(self._visible)
         }
 
