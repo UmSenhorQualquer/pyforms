6c4d08080944dc0a5fdab174a20527d4a0d380e1
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2016-05-12 14:22:51 +0100

    PEP8 compliance Auto-save changes if CTRL-S keys are pressed

--- pyforms/gui/Controls/ControlCodeEditor.py
+++ pyforms/gui/Controls/ControlCodeEditor.py
@@ -11,6 +11,7 @@
 @lastEditedBy: Carlos Mão de Ferro (carlos.maodeferro@neuro.fchampalimaud.org)
 '''
 
+import logging
 from pyforms.gui.Controls.ControlBase import ControlBase
 import pyforms.Utils.tools as tools
 from PyQt4 import uic, QtGui
@@ -18,8 +19,10 @@ from PyQt4.Qsci import QsciScintilla, QsciLexerPython  # pylint: disable=no-name
 from PyQt4.QtGui import QFontMetrics
 from PyQt4.QtGui import QFont
 from PyQt4.QtGui import QColor
+from PyQt4.QtCore import Qt
 
 from pyforms import conf
+
 
 class ControlCodeEditor(ControlBase):
     """
@@ -29,12 +32,12 @@ class ControlCodeEditor(ControlBase):
     ARROW_MARKER_NUM = 8
 
     def initForm(self):
+        self.logger = logging.getLogger(__name__)
+
         control_path = tools.getFileInSameDirectory(__file__, "code_editor.ui")
         self._form = uic.loadUi(control_path)
 
-        
         self._code_editor = self._form.code_editor
-
 
         self._save_button = self._form.save_button
         self._save_button.clicked[bool].connect(self.on_save_changes)
@@ -47,26 +50,27 @@ class ControlCodeEditor(ControlBase):
         self.form.font_size.addItem('18')
         self.form.font_size.addItem('24')
 
-        #Set the default font size
+        # Set the default font size
         index = self.form.font_size.findText(conf.CONTROL_CODE_EDITOR_DEFAULT_FONT_SIZE)
         self.form.font_size.setCurrentIndex(index)
 
         self.form.font_size.currentIndexChanged.connect(self.__font_size_index_changed)
-        
+
         icon = QtGui.QIcon()
-        icon.addPixmap( QtGui.qApp.style().standardPixmap(QtGui.QStyle.SP_DialogSaveButton), mode=QtGui.QIcon.Normal, state=QtGui.QIcon.On)
-        icon.addPixmap( QtGui.qApp.style().standardPixmap(QtGui.QStyle.SP_DialogSaveButton), mode=QtGui.QIcon.Normal, state=QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.qApp.style().standardPixmap(QtGui.QStyle.SP_DialogSaveButton), mode=QtGui.QIcon.Normal, state=QtGui.QIcon.On)
+        icon.addPixmap(QtGui.qApp.style().standardPixmap(QtGui.QStyle.SP_DialogSaveButton), mode=QtGui.QIcon.Normal, state=QtGui.QIcon.Off)
         self.form.save_button.setIcon(icon)
 
         self.lexer = QsciLexerPython
+
+        self._code_editor.keyPressEvent = self._key_pressed
 
         super(ControlCodeEditor, self).initForm()
 
     def __font_size_index_changed(self, index):
         item = self.form.font_size.currentText()
-        if len(item) >= 1: self._load_code_editor_settings()
-
-        
+        if len(item) >= 1:
+            self._load_code_editor_settings()
 
     def _load_code_editor_settings(self):
         """
@@ -153,11 +157,35 @@ class ControlCodeEditor(ControlBase):
         """
         On button save clicked, save changes made on the code editor to file
         """
-        if self.value is None: self._value = QtGui.QFileDialog.getSaveFileName(self.form, "Save file")       
-        with open(self.value, "w") as file: file.write(self._code_editor.text())
+        if self.value is None:
+            self._value = QtGui.QFileDialog.getSaveFileName(self.form, "Save file")
+        with open(self.value, "w") as file:
+            file.write(self._code_editor.text())
 
         self._code_editor.setModified(False)
         self._save_button.setEnabled(False)
+
+    def _key_pressed(self, event):
+        """
+        Handle KeyPressed event
+        We only care about CTRL-S in order to save changes
+        :param event: key event
+        """
+        QsciScintilla.keyPressEvent(self._code_editor, event)
+        if event.key() in [Qt.Key_S, Qt.Key_Save]:
+            modifiers = QtGui.QApplication.keyboardModifiers()
+            if modifiers == Qt.ControlModifier:
+                self.logger.debug("Saving...")
+                self.on_save_changes()
+
+        self.key_pressed(event)
+
+    def key_pressed(self, event):
+        """
+        Override KeyPressed event as you like
+        :param event: key event
+        """
+        pass
 
     ###################################################################
     ############ Properties ###########################################
@@ -165,23 +193,23 @@ class ControlCodeEditor(ControlBase):
 
     @property
     def lexer(self): return self._lexer
+
     @lexer.setter
-    def lexer(self, value): 
+    def lexer(self, value):
         self._lexer = value
         self._load_code_editor_settings()
 
-
     @property
     def value(self):
-        if len(self._value)==0: return None
+        if len(self._value) == 0:
+            return None
         return self._value
-
 
     @value.setter
     def value(self, value):
         ControlBase.value.fset(self, value)
-        if len(value)>0:
+        if len(value) > 0:
             with open(value, "r") as file:
-                self._code_editor.setText(str(file.read()))            
+                self._code_editor.setText(str(file.read()))
             self._code_editor.setModified(False)
             self._save_button.setEnabled(False)
