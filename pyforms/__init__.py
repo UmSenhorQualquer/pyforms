703f59e82e53ac61334453229048d0fa112ed5a2
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2016-08-12 18:26:07 +0100

    Fixes MAIN_APP constant

--- pyforms/__init__.py
+++ pyforms/__init__.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import os
 from pyforms import conf
 
 __author__ = "Ricardo Ribeiro"
@@ -13,27 +12,27 @@ __maintainer__ = "Ricardo Ribeiro"
 __email__ = "ricardojvr@gmail.com"
 __status__ = "Production"
 
+logger = logging.getLogger(__name__)
+
 if conf.PYFORMS_MODE in ['GUI', 'GUI-OPENCSP']:
 
-    from pyforms.gui import Controls
-    from pyforms.gui.BaseWidget import BaseWidget
+	from pyforms.gui import Controls
+	from pyforms.gui.BaseWidget import BaseWidget
 
-    if conf.PYFORMS_MODE in ['GUI-OPENCSP']:
-        from pyforms.gui.appmanager import startApp
-    else:
-        from pyforms.gui.standaloneManager import startApp
-        from pyforms.gui.standaloneManager import MAIN_APP
-
+	if conf.PYFORMS_MODE in ['GUI-OPENCSP']:
+		from pyforms.gui.appmanager import startApp
+	else:
+		from pyforms.gui.standaloneManager import startApp
 
 elif conf.PYFORMS_MODE in ['TERMINAL']:
 
-    from pyforms.terminal import Controls
-    from pyforms.terminal.BaseWidget import BaseWidget
-    from pyforms.terminal.appmanager import startApp
+	from pyforms.terminal import Controls
+	from pyforms.terminal.BaseWidget import BaseWidget
+	from pyforms.terminal.appmanager import startApp
 
 
 elif conf.PYFORMS_MODE in ['WEB']:
 
-    from pyforms_web.web import Controls
-    from pyforms_web.web.BaseWidget import BaseWidget
-    from pyforms_web.web.appmanager import startApp
+	from pyforms_web.web import Controls
+	from pyforms_web.web.BaseWidget import BaseWidget
+	from pyforms_web.web.appmanager import startApp
