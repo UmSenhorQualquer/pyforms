915dec27b5e5e4398b7bf07b7e2a2444a05b6f8a
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2016-09-07 16:06:52 +0100

    Clean build script

--- build_settings/win/build.bat
+++ build_settings/win/build.bat
@@ -5,13 +5,9 @@ setlocal enableextensions enabledelayedexpansion
 rem set /A PYTHON_VERSION=2
 
 IF /I "%PYTHON_VERSION%" EQU "2" (
-	ECHO "python 2"
-
 	set "WINPYDIR=C:\Users\swp\Python\WinPython-32bit-2.7.10.3\python-2.7.10"
 	set "WINPYVER=2.7.10.3"
 ) ELSE (
-	ECHO "python 3"
-
 	set "WINPYDIR=C:\Users\swp\Python\WinPython-32bit-3.4.3.7\python-3.4.3"
 	set "WINPYVER=3.4.3.7"
 )
