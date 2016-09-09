368b1e941173900c67af23fc85633d9bfc706dd8
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2016-09-09 15:20:23 +0100

    Simplify even more the ControlMdiArea Added documentation closeEvent.accept() can now be called since we manually create a subwindow without enabling the flag WA_DeleteOnClose

--- pyforms/gui/Controls/ControlMdiArea.py
+++ pyforms/gui/Controls/ControlMdiArea.py
@@ -7,6 +7,7 @@ import logging
 
 from PyQt4 import QtCore
 from PyQt4.QtGui import QMdiArea
+from PyQt4.QtGui import QMdiSubWindow
 
 from pyforms.gui.Controls.ControlBase import ControlBase
 
@@ -31,67 +32,64 @@ class ControlMdiArea(ControlBase, QMdiArea):
 	def __init__(self, label=""):
 		QMdiArea.__init__(self)
 		ControlBase.__init__(self, label)
-		#		self._value = []
 		self._showCloseButton = True
 
 		self.logger = logging.getLogger(__name__)
 
-	def __flags(self):
-		flags = QtCore.Qt.SubWindow
-
-		flags |= QtCore.Qt.WindowTitleHint
-		flags |= QtCore.Qt.WindowMinimizeButtonHint
-		flags |= QtCore.Qt.WindowMaximizeButtonHint
-		if self._showCloseButton:
-			flags |= QtCore.Qt.WindowSystemMenuHint
-			flags |= QtCore.Qt.WindowCloseButtonHint
-
-		return flags
-
-	def __sub__(self, window):
-		window.close()
+	def __sub__(self, widget):
+		"""
+		Remove subwindow and unassigned it from widget
+		:param widget:
+		:return:
+		"""
+		widget.close()
+		self.removeSubWindow(widget.subwindow)
+		del widget.subwindow
 		return self
 
 	def __add__(self, widget):
+		"""
+		Show widget on mdi area.
 
-		# if not widget._formLoaded:
-		# 	widget.initForm()
+		If widget does not have a subwindow assigned, create a new subwindow without enabling the WA_DeleteOnClose event.
+		This will allow subwindow to be hidden instead of destroyed. Otherwise, the closeEvent.accept() will cause
+		the "Internal c++ Object Already Deleted" problem.
+
+		If widget already has a subwindow, just show them (both the subwindow and the widget inside)!
+		:param widget:
+		:return:
+		"""
 
 		if not hasattr(widget, 'subwindow'):
-			widget.subwindow = self.addSubWindow(widget)
-			widget.subwindow.overrideWindowFlags(self.__flags())
+			subwindow = QMdiSubWindow()
+			subwindow.setWidget(widget)
+			# DO NOT SET ATTRIBUTE WA_DeleteOnClose because we want window not to be destroyed
+			widget.subwindow = self.addSubWindow(subwindow)
 		else:
 			widget.subwindow.show()
 
-		# self.value.append(other)
-
 		widget.show()
 		widget.closeEvent = lambda x: self._subWindowClosed(x)
-
 		widget.setFocus()
+
 		logger.debug("Sub window opened. MDI area sub windows: %s", self.subWindowList())
+
 		return self
 
 	def _subWindowClosed(self, closeEvent):
+		"""
+		Perform actions when subwindow is closed.
+		In this case, we don't want subwindow to be removed nor destroyed in order to reutilize later.
+		The closeEvent.accept() will just hide the subwindow.
+		:param closeEvent:
+		:return:
+		"""
 
 		window = self.activeSubWindow()
 		widget = window.widget()
-
-		# self.removeSubWindow(window)
-
-		closeEvent.ignore()
+		# self.removeSubWindow(window) DO NOT REMOVE TO KEEP WINDOW POSITION
 		widget.beforeClose()
-		window.hide()
-
-		# If beforeClose return False, will just hide the window.
-		# If return True or None remove it from the mdi area
-		# res = activeWidget.beforeClose()
-
-
-
-		# if res is None or res is True:
-		# 	if activeWidget in self._value:
-		# 		self._value.remove(activeWidget)
+		closeEvent.accept()
 
 		logger.debug("Sub window closed. MDI area sub windows: %s", self.subWindowList())
 
@@ -118,20 +116,3 @@ class ControlMdiArea(ControlBase, QMdiArea):
 	@property
 	def form(self):
 		return self
-
-	# @property
-	# def value(self):
-	# 	return ControlBase.value.fget(self)
-	#
-	# @value.setter
-	# def value(self, value):
-	# 	self.closeAllSubWindows()
-	# 	self._value = []
-	#
-	# 	if isinstance(value, list):
-	# 		for w in value:
-	# 			self += w
-	# 	else:
-	# 		self += value
-	#
-	# 	ControlBase.value.fset(self, self._value)
