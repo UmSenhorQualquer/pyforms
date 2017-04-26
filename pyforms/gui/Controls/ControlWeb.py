ce426834376c2f4eb60f3649ccc5a92fed9e2aec
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2017-04-26 17:59:52 +0100

    Fixes import error on ControlWeb for PyQt5

--- pyforms/gui/Controls/ControlWeb.py
+++ pyforms/gui/Controls/ControlWeb.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__author__      = "Ricardo Ribeiro"
-__credits__     = ["Ricardo Ribeiro"]
-__license__     = "MIT"
-__version__     = "0.0"
-__maintainer__  = "Ricardo Ribeiro"
-__email__       = "ricardojvr@gmail.com"
-__status__      = "Development"
-
-
-
-
+__author__ = "Ricardo Ribeiro"
+__credits__ = ["Ricardo Ribeiro"]
+__license__ = "MIT"
+__version__ = "0.0"
+__maintainer__ = "Ricardo Ribeiro"
+__email__ = "ricardojvr@gmail.com"
+__status__ = "Development"
 
 from pysettings import conf
 
 if conf.PYFORMS_USE_QT5:
-	from PyQt5.QtWebKitWidgets import QWebView¶, QVBoxLayout
+	from PyQt5.QtWebEngineWidgets import QWebEngineView
 	from PyQt5.QtCore import QUrl
 else:
 	from PyQt4.QtCore import QUrl
@@ -24,10 +20,10 @@ else:
 
 from pyforms.gui.Controls.ControlBase import ControlBase
 
-class ControlWeb(ControlBase, QWebView):
 
-	def __init__(self, label = ""):
-		QWebView.__init__(self)
+class ControlWeb(ControlBase, QWebEngineView):
+	def __init__(self, label=""):
+		QWebEngineView.__init__(self)
 		ControlBase.__init__(self, label)
 
 	############################################################################
@@ -36,10 +32,12 @@ class ControlWeb(ControlBase, QWebView):
 
 	@property
 	def value(self): return ControlBase.value.fget(self)
+
 	@value.setter
 	def value(self, value):
 		ControlBase.value.fset(self, value)
-		QWebView.load(self, QUrl(value) )
+		QWebEngineView.load(self, QUrl(value))
 
 	@property
-	def form(self): return self
+	def form(self):
+		return self
